faf5f25262d23b91b751783c9308570c1e787908
Matt Goldsworthy <matt.j.goldsworthy@gmail.com>
2018-10-17 23:31:18 -0600

    added authentication

--- my_website/blog/urls.py
+++ my_website/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    url(r'^accounts/', include('django.contrib.auth.urls'), name='login'),
 ]
